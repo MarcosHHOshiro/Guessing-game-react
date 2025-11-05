8517f5a2973347222032979ba3a0cec7c6ca8126
Marcos <marcos.2h.oshiro@gmail.com>
2025-11-04 20:46:21 -0400

    Creating header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
+import styles from './app.module.css'
 import { Header } from "./components/Header"
 
 function App() {
 
   return (
-    <div>
-      <Header />
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
     </div>
   )
 }
